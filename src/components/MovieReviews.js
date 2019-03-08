b337efb9b592b2578f8c2e5838238c8177190c05
ValerieHMoore <periwinkle17@outlook.com>
2019-03-08 17:28:25 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => 
+    <div className="review-list">{props.reviews.map((review) =>
+        <ul className="review">
+            <h3>{<a href={review.link.url} >{review.headline}</a>}</h3>
+        </ul>
+    )}
+    </div>
+
+export default MovieReviews
+
+// You are free to lay out each review as you like using the data that the API returns, but make sure that the component outputs a top-level element with the class review-list and that each review is wrapped by an element with class review.
